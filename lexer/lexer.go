c6e4cbf0d4a87e50ef82cc1f43c510225db18841
Murilo Henrique <murilo.alves@flashapp.com.br>
2023-09-21 03:11:44 -0300

    feat: add lexer and parser

---
+++ lexer/lexer.go
@@ -0,0 +1,227 @@
+package lexer
+
+import (
+	"fmt"
+
+	"github.com/davyxu/golexer"
+	"github.com/omurilo/rinha-compiler/ast"
+	// "github.com/omurilo/rinha-compiler/runtime"
+)
+
+var KEYWORDS = map[string]string{
+	"print":  ":PRINT",
+	"true":   ":TRUE",
+	"false":  ":FALSE",
+	"first":  ":FIRST",
+	"second": ":SECOND",
+	"if":     ":IF",
+	"else":   ":ELSE",
+	"fn":     ":FUNCTION",
+	"let":    ":LET",
+}
+
+var SYMBOLS = map[string]string{
+	"(":  ":LPAREN",
+	")":  ":RPAREN",
+	"+":  ":BINARY_OP",
+	"-":  ":BINARY_OP",
+	"*":  ":BINARY_OP",
+	"/":  ":BINARY_OP",
+	"%":  ":BINARY_OP",
+	"==": ":BINARY_OP",
+	"<":  ":BINARY_OP",
+	">":  ":BINARY_OP",
+	"=":  ":ASSIGNMENT",
+	";":  ":SEMICOLON",
+	"{":  ":LBRACE",
+	"}":  ":RBRACE",
+	",":  ":COMMA",
+	"=>": ":ARROW",
+	">=": ":BINARY_OP",
+	"<=": ":BINARY_OP",
+	"!=": ":BINARY_OP",
+	"&&": ":BINARY_OP",
+	"||": ":BINARY_OP",
+}
+
+type Token struct {
+	Type     string
+	Value    string
+	Location ast.Location
+}
+
+const (
+	Token_EOF = iota
+	Token_Unknown
+	Token_Numeral
+	Token_String
+	Token_WhiteSpace
+	Token_LineEnd
+	Token_UnixStyleComment
+	Token_Identifier
+	Token_Semicolon
+	Token_If
+	Token_Else
+	Token_True
+	Token_False
+	Token_First
+	Token_Second
+	Token_Print
+	Token_Add
+	Token_Sub
+	Token_Mul
+	Token_Div
+	Token_Rem
+	Token_Eq
+	Token_Let
+	Token_Fn
+	Token_Comma
+	Token_Lte
+	Token_Gte
+	Token_Lt
+	Token_Gt
+	Token_Neq
+	Token_Or
+	Token_And
+	Token_Assignment
+	Token_Arrow
+	Token_LParen
+	Token_RParen
+	Token_LBrace
+	Token_RBrace
+)
+
+type CustomParser struct {
+	*golexer.Parser
+}
+
+func NewCustomParser(filename string) *CustomParser {
+	l := golexer.NewLexer()
+
+	l.AddMatcher(golexer.NewNumeralMatcher(Token_Numeral))
+	l.AddMatcher(golexer.NewStringMatcher(Token_String))
+
+	l.AddIgnoreMatcher(golexer.NewWhiteSpaceMatcher(Token_WhiteSpace))
+	l.AddIgnoreMatcher(golexer.NewLineEndMatcher(Token_LineEnd))
+	l.AddIgnoreMatcher(golexer.NewUnixStyleCommentMatcher(Token_UnixStyleComment))
+
+	l.AddMatcher(golexer.NewSignMatcher(Token_Semicolon, ";"))
+	l.AddMatcher(golexer.NewSignMatcher(Token_Comma, ","))
+
+	l.AddMatcher(golexer.NewKeywordMatcher(Token_If, "if"))
+	l.AddMatcher(golexer.NewKeywordMatcher(Token_Else, "else"))
+	l.AddMatcher(golexer.NewKeywordMatcher(Token_Print, "print"))
+	l.AddMatcher(golexer.NewKeywordMatcher(Token_First, "first"))
+	l.AddMatcher(golexer.NewKeywordMatcher(Token_Second, "second"))
+	l.AddMatcher(golexer.NewKeywordMatcher(Token_Let, "let"))
+	l.AddMatcher(golexer.NewKeywordMatcher(Token_Fn, "fn"))
+	l.AddMatcher(golexer.NewKeywordMatcher(Token_True, "true"))
+	l.AddMatcher(golexer.NewKeywordMatcher(Token_False, "false"))
+
+	l.AddMatcher(golexer.NewSignMatcher(Token_Add, "+"))
+	l.AddMatcher(golexer.NewSignMatcher(Token_Sub, "-"))
+	l.AddMatcher(golexer.NewSignMatcher(Token_Mul, "*"))
+	l.AddMatcher(golexer.NewSignMatcher(Token_Div, "/"))
+	l.AddMatcher(golexer.NewSignMatcher(Token_Rem, "%"))
+	l.AddMatcher(golexer.NewSignMatcher(Token_Arrow, "=>"))
+	l.AddMatcher(golexer.NewSignMatcher(Token_Lt, "<"))
+	l.AddMatcher(golexer.NewSignMatcher(Token_Gt, ">"))
+	l.AddMatcher(golexer.NewSignMatcher(Token_Lte, "<="))
+	l.AddMatcher(golexer.NewSignMatcher(Token_Gte, ">="))
+	l.AddMatcher(golexer.NewSignMatcher(Token_Eq, "=="))
+	l.AddMatcher(golexer.NewSignMatcher(Token_Neq, "!="))
+	l.AddMatcher(golexer.NewSignMatcher(Token_Assignment, "="))
+	l.AddMatcher(golexer.NewSignMatcher(Token_Or, "||"))
+	l.AddMatcher(golexer.NewSignMatcher(Token_And, "&&"))
+
+	l.AddMatcher(golexer.NewSignMatcher(Token_LParen, "("))
+	l.AddMatcher(golexer.NewSignMatcher(Token_RParen, ")"))
+	l.AddMatcher(golexer.NewSignMatcher(Token_LBrace, "{"))
+	l.AddMatcher(golexer.NewSignMatcher(Token_RBrace, "}"))
+
+	l.AddMatcher(golexer.NewIdentifierMatcher(Token_Identifier))
+
+	l.AddMatcher(golexer.NewUnknownMatcher(Token_Unknown))
+
+	return &CustomParser{
+		Parser: golexer.NewParser(l, filename),
+	}
+}
+
+func Initialize(program string, filename string) *CustomParser {
+	p := NewCustomParser(filename)
+	p.Lexer().Start(program)
+
+	return p
+}
+
+func (p *CustomParser) Next() Token {
+	var token Token
+	p.NextToken()
+
+	if p.TokenID() != 0 {
+		switch p.MatcherName() {
+		case "NumeralMatcher":
+			token = Token{Type: ":NUMBER", Value: p.TokenValue(), Location: parse_location(p.TokenPos())}
+		case "StringMatcher":
+			token = Token{Type: ":STRING", Value: p.TokenValue(), Location: parse_location(p.TokenPos())}
+		case "SignMatcher":
+			token = Token{Type: SYMBOLS[p.TokenValue()], Value: p.TokenValue(), Location: parse_location(p.TokenPos())}
+		case "KeywordMatcher":
+			token = Token{Type: KEYWORDS[p.TokenValue()], Value: p.TokenValue(), Location: parse_location(p.TokenPos())}
+		case "IdentifierMatcher":
+			token = Token{Type: ":IDENTIFIER", Value: p.TokenValue(), Location: parse_location(p.TokenPos())}
+		}
+	}
+
+	return token
+}
+
+func (p *CustomParser) Tokenize() {
+	token := p.Next()
+
+	for p.TokenID() != 0 {
+		fmt.Println(token)
+		token = p.Next()
+	}
+}
+
+func parse_location(position golexer.TokenPos) ast.Location {
+	return ast.Location{Filename: position.SourceName, Start: uint32(position.Line), End: uint32(position.Col)}
+}
+
+// func parse_trash(token string) bool {
+// 	trash_regex := regexp.MustCompile(`\s+|//[^\n\r]*[\n\r]*|/\*[^*]*\*+(?:[^/*][^*]*\*+)*/`)
+// 	ok := trash_regex.MatchString(token)
+// 	return ok
+// }
+//
+// func parse_identifier(token string) bool {
+// 	identifier_regex := regexp.MustCompile(`[a-zA-Z][a-zA-Z0-9/_]*`)
+// 	ok := identifier_regex.MatchString(token)
+// 	return ok
+// }
+//
+// func parse_strings(token string) bool {
+// 	string_regex := regexp.MustCompile(`"(\\\\|\\"|[^"\\])*"`)
+// 	ok := string_regex.MatchString(token)
+// 	return ok
+// }
+//
+// func parse_numbers(token string) bool {
+// 	number_regex := regexp.MustCompile(`\d+`)
+// 	ok := number_regex.MatchString(token)
+// 	return ok
+// }
+//
+// func parse_symbols(token string) bool {
+// 	symbols_regex := regexp.MustCompile(`[\(\)\+\-\*\/\<\>\;\{\}\,]|\!?\=\=?\>?|\|\||\&\&`)
+// 	ok := symbols_regex.MatchString(token)
+// 	return ok
+// }
+//
+// func parse_keywords(token string) bool {
+// 	keywords_regex := regexp.MustCompile(`^(print|first|second|true|false|if|else|fn|let)`)
+// 	ok := keywords_regex.MatchString(token)
+// 	return ok
+// }
---
+++ parser/parser.go
@@ -0,0 +1,240 @@
+package parser
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/omurilo/rinha-compiler/lexer"
+	"github.com/omurilo/rinha-compiler/runtime"
+)
+
+var BINARY_OPERATIONS = map[string]string{
+	"+":  "Add",
+	"-":  "Sub",
+	"*":  "Mul",
+	"/":  "Div",
+	"%":  "Rem",
+	"==": "Eq",
+	"<":  "Lt",
+	">":  "Gt",
+	">=": "Gte",
+	"<=": "Lte",
+	"!=": "Neq",
+	"&&": "And",
+	"||": "Or",
+}
+
+var tree map[string]interface{}
+var current_token lexer.Token
+var p *lexer.CustomParser
+
+func Main(program string, filename string) string {
+	p = lexer.Initialize(program, filename)
+
+	advance()
+	tree = map[string]interface{}{"name": filename, "expression": parser()}
+	treeJson, _ := json.Marshal(tree)
+
+	// err != nil {
+	//   runtime.Error(current_token.Location, "unexpected error occurred at parsing tree to json")
+	// }
+
+	fmt.Println(string(treeJson))
+	return string(treeJson)
+}
+
+func parser() map[string]interface{} {
+	switch current_token.Type {
+	case ":PRINT":
+		return parse_print()
+	case ":STRING":
+		return maybe_binary_op(parse_string())
+	case ":NUMBER":
+		return maybe_binary_op(parse_number())
+	case ":IDENTIFIER":
+		return maybe_binary_op(parse_identifier())
+	case ":LET":
+		return parse_let()
+	case ":FUNCTION":
+		return parse_function()
+	case ":IF":
+		return parse_if()
+	case ":TRUE":
+		return parse_bool()
+	case ":FALSE":
+		return parse_bool()
+	default:
+		// consume(current_token.Type)
+		return nil
+	}
+}
+
+func parse_print() map[string]interface{} {
+	node := map[string]interface{}{"kind": "Print"}
+
+	consume(":PRINT")
+	consume(":LPAREN")
+	node["value"] = parser()
+
+	consume(":RPAREN")
+
+	return node
+}
+
+func parse_string() map[string]interface{} {
+	node := map[string]interface{}{"kind": "Str", "value": current_token.Value, "location": current_token.Location}
+	consume(":STRING")
+
+	return node
+}
+
+func parse_number() map[string]interface{} {
+	number, _ := strconv.Atoi(current_token.Value)
+	node := map[string]interface{}{"kind": "Int", "value": number, "location": current_token.Location}
+	consume(":NUMBER")
+
+	return node
+}
+
+func parse_let() map[string]interface{} {
+	node := map[string]interface{}{"kind": "Let", "location": current_token.Location}
+	node["name"] = map[string]interface{}{"text": nil}
+	node["value"] = make(map[string]interface{})
+	node["next"] = make(map[string]interface{})
+	consume(":LET")
+
+	node["name"].(map[string]interface{})["text"] = current_token.Value
+	consume(":IDENTIFIER")
+	consume(":ASSIGNMENT")
+
+	node["value"] = parser()
+	consume(":SEMICOLON")
+	node["next"] = parser()
+
+	return node
+}
+
+func parse_function() map[string]interface{} {
+	node := map[string]interface{}{"kind": "Function", "location": current_token.Location}
+	node["parameters"] = []map[string]interface{}{}
+	node["value"] = make(map[string]interface{})
+
+	consume(":FUNCTION")
+	consume(":LPAREN")
+
+	for current_token.Type != ":RPAREN" {
+		parameter := map[string]interface{}{"text": current_token.Value, "location": current_token.Location}
+		node["parameters"] = append(node["parameters"].([]map[string]interface{}), parameter)
+		consume(":IDENTIFIER")
+		if current_token.Type == ":COMMA" {
+			consume(":COMMA")
+		}
+	}
+
+	consume(":RPAREN")
+	consume(":ARROW")
+	consume(":LBRACE")
+
+	node["value"] = parser()
+
+	consume(":RBRACE")
+
+	return node
+}
+
+func parse_if() map[string]interface{} {
+	node := map[string]interface{}{"kind": "If", "location": current_token.Location}
+	node["then"] = map[string]interface{}{}
+	node["otherwise"] = map[string]interface{}{}
+	node["condition"] = map[string]interface{}{}
+
+	consume(":IF")
+	consume(":LPAREN")
+
+	node["condition"] = parser()
+
+	consume(":RPAREN")
+	consume(":LBRACE")
+
+	node["then"] = parser()
+
+	consume(":RBRACE")
+
+	if current_token.Type == ":ELSE" {
+		consume(":ELSE")
+		consume(":LBRACE")
+
+		node["otherwise"] = parser()
+
+		consume(":RBRACE")
+	}
+
+	return node
+}
+
+func parse_bool() map[string]interface{} {
+	node := map[string]interface{}{"kind": "Bool", "value": current_token.Value == "true", "location": current_token.Location}
+	consume(fmt.Sprintf(":%s", strings.ToUpper(current_token.Value)))
+
+	return node
+}
+
+func parse_identifier() map[string]interface{} {
+	node := map[string]interface{}{"kind": "Var", "text": current_token.Value, "location": current_token.Location}
+	consume(":IDENTIFIER")
+
+	if current_token.Type == ":LPAREN" {
+		consume(":LPAREN")
+		function_call := maybe_binary_op(parse_function_call(node))
+		consume(":RPAREN")
+
+		return function_call
+	}
+
+	return node
+}
+
+func parse_function_call(callee map[string]interface{}) map[string]interface{} {
+	node := map[string]interface{}{"kind": "Call", "callee": callee, "location": current_token.Location}
+	node["arguments"] = []map[string]interface{}{}
+
+	for current_token.Type != ":RPAREN" {
+		argument := parser()
+		node["arguments"] = append(node["arguments"].([]map[string]interface{}), argument)
+		if current_token.Type == ":COMMA" {
+			consume(":COMMA")
+		}
+	}
+
+	return node
+}
+
+func maybe_binary_op(lhs map[string]interface{}) map[string]interface{} {
+	if current_token.Type != ":BINARY_OP" {
+		return lhs
+	}
+
+	node := map[string]interface{}{"kind": "Binary", "op": BINARY_OPERATIONS[current_token.Value], "lhs": lhs, "location": current_token.Location}
+
+	consume(":BINARY_OP")
+	node["rhs"] = parser()
+
+	return node
+}
+
+func advance() {
+	current_token = p.Next()
+}
+
+func consume(token_type string) {
+	if current_token.Value == "" {
+		runtime.Error(current_token.Location, fmt.Sprintf("Expected %v but found nil", token_type))
+	}
+
+	if current_token.Type != token_type {
+		runtime.Error(current_token.Location, fmt.Sprintf("Expected %v but found %v in %v", token_type, current_token.Type, current_token.Value))
+	}
+	advance()
+}
