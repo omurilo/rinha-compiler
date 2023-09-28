4a244df5b3089e1372a7f88aa97ac44eef90cb8d
Murilo Henrique <murilo.alves@flashapp.com.br>
2023-09-28 20:29:10 -0300

    test: parse all rinha files to json before run tests

--- eval_test.go
+++ eval_test.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"os"
+	"os/exec"
 	"strings"
 	"testing"
 )
@@ -10,12 +13,23 @@ import (
 func TestExamples(t *testing.T) {
 	files, _ := os.ReadDir("examples")
 	for _, file := range files {
-		t.Log(file.Name())
-		if strings.Contains(file.Name(), ".rinha") {
+		fileName := file.Name()
+		t.Log(fileName)
+		if !strings.Contains(fileName, ".rinha.json") {
+			rinhaToJson(fileName)
+			continue
+		}
+	}
+
+	files, _ = os.ReadDir("examples")
+	for _, file := range files {
+		fileName := file.Name()
+		t.Log(fileName)
+		if !strings.Contains(fileName, ".rinha.json") {
 			continue
 		}
 
-		stdin, err := os.ReadFile("examples/" + file.Name())
+		stdin, err := os.ReadFile("examples/" + fileName)
 
 		if err != nil {
 			panic(err)
@@ -23,13 +37,34 @@ func TestExamples(t *testing.T) {
 
 		var ast File
 		if err := json.Unmarshal(stdin, &ast); err != nil {
+			fmt.Println(stdin)
 			t.Error("Error decoding JSON:", err)
 			return
 		}
 		SCOPE_DEFAULT_SIZE := 8
 		scope := make(Scope, SCOPE_DEFAULT_SIZE)
-		t.Log(">>>>>>>>>>>> ", file.Name())
+		t.Log(">>>>>>>>>>>> ", fileName)
 		Eval(scope, ast.Expression)
 		t.Log()
 	}
+}
+
+func rinhaToJson(fileName string) {
+	cmd := exec.Command("rinha", fmt.Sprintf("examples/%s", fileName))
+	value, err := cmd.Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f, err := os.Create(fmt.Sprintf("examples/%s.json", fileName))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a, err := f.WriteString(string(value))
+	if err != nil {
+		log.Fatal(a, err)
+	}
+	f.Sync()
 }
